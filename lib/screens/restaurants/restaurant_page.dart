55febf5fa9659ba7cc6b094e921ef6c3c0ee3ce3
Diego H. Piva <diego_pivanp@hotmail.com>
2021-09-22 13:06:51 -0300

    criada pagina de restaurantes

---
+++ lib/screens/restaurants/restaurant_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import './widgets/restaurant_card.dart';
+import '../../models/Restaurant.dart';
+import '../../widgets/botton_navigator.dart';
+
+class RestaurantsPage extends StatefulWidget {
+  RestaurantsPage({Key key}) : super(key: key);
+
+  @override
+  _RestaurantsPageState createState() => _RestaurantsPageState();
+}
+
+class _RestaurantsPageState extends State<RestaurantsPage> {
+  List<Restaurant> _restaurants = [
+    new Restaurant(
+        name: 'Carneiro no Ponto',
+        contact: 'carneiro@teste.com',
+        image: '',
+        uuid: 'fafdafaadfa'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 2',
+        contact: 'carneiro2@teste.com',
+        image: '',
+        uuid: 'adfqfadfqsd'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 3',
+        contact: 'carneiro3@teste.com',
+        image: '',
+        uuid: 'fqfasrqcaafa'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 3',
+        contact: 'carneiro3@teste.com',
+        image: '',
+        uuid: 'fqfasrqcaafa'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 3',
+        contact: 'carneiro3@teste.com',
+        image: '',
+        uuid: 'fqfasrqcaafa'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 3',
+        contact: 'carneiro3@teste.com',
+        image: '',
+        uuid: 'fqfasrqcaafa'),
+    new Restaurant(
+        name: 'Carneiro no Ponto 3',
+        contact: 'carneiro3@teste.com',
+        image: '',
+        uuid: 'fqfasrqcaafa'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Restaurantes'),
+        centerTitle: true,
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: _buildRestaurants(context),
+      bottomNavigationBar: BottonNavigatorCurved(0),
+    );
+  }
+
+  Widget _buildRestaurants(context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: _restaurants.length,
+        itemBuilder: (context, index) {
+          final Restaurant restaurant = _restaurants[index];
+
+          return RestaurantCard(
+            uuid: restaurant.uuid,
+            name: restaurant.name,
+            image: restaurant.image,
+            contact: restaurant.contact,
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/restaurants/widgets/restaurant_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../widgets/show_image_cached_network.dart';
+
+class RestaurantCard extends StatelessWidget {
+  final String uuid;
+  final String name;
+  final String image;
+  final String contact;
+
+  RestaurantCard({this.uuid, this.name, this.image, this.contact});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print(this.name);
+        Navigator.pushNamed(context, '/foods');
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 4, right: 1, left: 1),
+        child: Card(
+          elevation: 2.5,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[200]),
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+                color: Colors.white),
+            child: Padding(
+              padding: EdgeInsets.all(4),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    height: 60,
+                    child: ShowImageCachedNetwork(
+                        'https://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png'),
+                  ),
+                  VerticalDivider(color: Colors.black26),
+                  Expanded(
+                      child: Text(
+                    name,
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
