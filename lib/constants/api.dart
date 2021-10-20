43f77294f2bbe7df3d0dd7ab62c06298f119d374
Diego Henrique Piva <diego_pivanp@hotmail.com>
2021-10-20 11:38:44 -0300

    alterado url da api

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,4 +1,4 @@
-const API_URL = 'http://10.0.2.2/api/';
+const API_URL = 'https://api.dimyfood.com.br/api/';
 const API_VERSION = 'v1';
 const API_RECEIVE_TIMEOUT = 15000;
 const API_CONNECTION_TIMEOUT = 15000;
